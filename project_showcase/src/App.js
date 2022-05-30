26eed2ad85cb3d0349faac40d47a7de36c76d934
Aysan Isayo <aisayo@icloud.com>
2022-05-30 16:49:25 -0400

    added solution branch for lecture 3

--- project_showcase/src/App.js
+++ project_showcase/src/App.js
@@ -1,14 +1,25 @@
+import React, { useState } from "react";
 import Header from "./components/Header";
 import ProjectForm from "./components/ProjectForm";
 import ProjectList from "./components/ProjectList";
 
-import projects from "./projects";
-
 const App = () => {
+  const [projects, setProjects] = useState([]);
+  const [isDarkMode, setIsDarkMode] = useState(true);
+
+  const handleClick = () => {
+    fetch("http://localhost:4000/projects")
+      .then((res) => res.json())
+      .then((projects) => setProjects(projects));
+  };
+
+  const onToggleDarkMode = () => setIsDarkMode(!isDarkMode);
+
   return (
-    <div className="App">
-      <Header />
+    <div className={isDarkMode ? "App" : "App light"}>
+      <Header isDarkMode={isDarkMode} onToggleDarkMode={onToggleDarkMode} />
       <ProjectForm />
+      <button onClick={handleClick}>Load Projects</button>
       <ProjectList projects={projects} />
     </div>
   );
